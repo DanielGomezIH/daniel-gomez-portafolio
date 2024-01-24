7e1347d9c11501adeedf6b2bda5a938aca107818
Daniel Gómez <danielivgho5@gmail.com>
2024-01-24 18:27:38 -0500

    First commit

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+import { navLinksRef } from './FirebaseStore';
+import { servicesRef } from './FirebaseStore';
+import { technologiesRef } from './FirebaseStore';
+import { experiencesRef } from './FirebaseStore';
+import { testimonialsRef } from './FirebaseStore';
+import { projectsRef } from './FirebaseStore';
+
+export {
+  navLinksRef,
+  servicesRef,
+  technologiesRef,
+  experiencesRef,
+  testimonialsRef,
+  projectsRef,
+};
